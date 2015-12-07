fe2f27d0de80bdc47f53aa7244cb5dd8c00af35a
machinefixer <waywardson.wayne@gmail.com>
2015-12-07 20:35:06 +0800

    update

--- YahooWeather/YWCondition.h
+++ YahooWeather/YWCondition.h
@@ -15,14 +15,14 @@
 @property (nonatomic, strong) NSNumber *temperature;
 @property (nonatomic, strong) NSNumber *tempHigh;
 @property (nonatomic, strong) NSNumber *tempLow;
-@property (nonatomic, copy) NSString *locationName;
+@property (nonatomic, strong) NSString *locationName;
 @property (nonatomic, strong) NSDate *sunrise;
 @property (nonatomic, strong) NSDate *sunset;
-@property (nonatomic, copy) NSString *conditionDescription;
-@property (nonatomic, copy) NSString *condition;
+@property (nonatomic, strong) NSString *conditionDescription;
+@property (nonatomic, strong) NSString *condition;
 @property (nonatomic, strong) NSNumber *windBearing;
 @property (nonatomic, strong) NSNumber *windSpeed;
-@property (nonatomic, copy) NSString *icon;
+@property (nonatomic, strong) NSString *icon;
 
 // 将天气图标和天气状况对应起来
 - (NSString *)imageName;
